639a8c1545b9f10e13e0e577666a112cedfc4fda
Christopher Wright <chrisw@ChrisW-MBP-140.local>
2018-02-12 21:06:09 +1100

    Use prettier to format html

--- htmlsnapshot.js
+++ htmlsnapshot.js
@@ -2,25 +2,30 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const argv = require('yargs').argv;
-const beautify_html = require('js-beautify').html;
+const prettier = require('prettier');
 
 (async () => {
-  const { o: output, u: url, s: selector } = argv;
+    const { o: output, u: url, s: selector } = argv;
 
-  if (!url || !output || !selector) {
-    return;
-  }
+    if (!url || !output || !selector) {
+        return;
+    }
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  await page.waitForSelector(selector);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.waitForSelector(selector);
 
-  const html = await page.content();
-  const $ = cheerio.load(html);
-  const component = $(selector);
-  fs.writeFileSync(output, $.html(component), 'utf8');
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const component = $(selector);
+    const componentHtml = $.html(component);
+    const prettierFormatedHtml = prettier
+        .format(componentHtml, { semi: false })
+        .slice(1);
 
-  browser.close();
+    fs.writeFileSync(output, prettierFormatedHtml, 'utf8');
+
+    browser.close();
 })();
